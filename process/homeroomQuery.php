35bb6fda220c656b935696e19c5bf8c78db1d540
Brandi Besalke <atrejub@gmail.com>
2014-06-27 12:19:52 -0500

    Update homeroomQuery.php

--- process/homeroomQuery.php
+++ process/homeroomQuery.php
@@ -1,9 +1,21 @@
 <?php
+
+/*
+	This outputs a list of students in a given homeroom, either as a tabled list or as select box elements, depending on what is passed to here.
+*/
+
+
+// FROM HERE (until below where I say UNTIL HERE) is the intranet user authentication stuff which would need changing 
+
 $path = $_SERVER['DOCUMENT_ROOT'] . '/peds/pass/includes/catnetapps.inc';
 require_once $path;
 
 // instantiate class
 $catn= NEW CatnetApps();
+
+$catn->dbconnect("catnet",$catn->dbuser,$catn->dbpassword);
+
+// UNTIL HERE - note that this includes the database connection, so that would have to be redone as well
 
 $theTeacher = $catn->getVariable("teacher");
 $outputType = $catn->getVariable("outputType");
@@ -15,7 +27,6 @@ if ($outputType == "selectBox") {
 	$studentList = "<select id='theStudent'><option value='0' selected>Select a Student:</option>";
 }
 
-$catn->dbconnect("catnet",$catn->dbuser,$catn->dbpassword);
 //query to get the students (id + names) that are in particular teacher's homeroom
 $query = "select homeroom.userIDStudent, user.lastName, user.firstName from pass.homeroom, catnet.user where homeroom.userIDTeacher=".$theTeacher." and homeroom.userIDStudent=user.userID order by user.lastName, user.firstName";
 $result = mysql_query($query);
