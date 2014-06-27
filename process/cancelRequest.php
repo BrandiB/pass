1e1d9a5dd573bfa6c340b6361538c71487997149
Brandi Besalke <atrejub@gmail.com>
2014-06-27 11:59:48 -0500

    Update cancelRequest.php

--- process/cancelRequest.php
+++ process/cancelRequest.php
@@ -1,5 +1,11 @@
 <?php
 
+/*
+	This cancels a previously-made request.
+*/
+
+// FROM HERE (until below where I say UNTIL HERE) is the intranet user authentication stuff which would need changing 
+
 $path = $_SERVER['DOCUMENT_ROOT'] . '/peds/pass/includes/catnetapps.inc';
 require_once $path;
 
@@ -8,9 +14,12 @@ $catn= NEW CatnetApps();
 $keyid = $catn->getVariable("keyid");
 $catn->setUserInfo($keyid);
 
+$catn->dbconnect("catnet",$catn->dbuser,$catn->dbpassword);
+
+// UNTIL HERE - note that this includes the database connection, so that would have to be redone as well
+
 $whichRequest = $catn->getVariable("whichRequest");
 
-$catn->dbconnect("catnet",$catn->dbuser,$catn->dbpassword);
 
 // Delete request from database
 $query = "delete from pass.requestmatrix where requestID=".$whichRequest;
